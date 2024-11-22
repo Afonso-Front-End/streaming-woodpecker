313579437bc7c17001f9631c9223e6c56d5d5c97
Afonso-Front-End <afonsogithub@gmail.com>
2024-11-22 11:07:39 -0300

    atualizando

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./Layout.css"
 
-import Emphasis from "../components/emphasis/Emphasis";
+
 import MediaGallery from "../components/MediaGallery/MediaGallery";
 
 const Layout = () => {
     return (
         <div className="container">
             <div className="content">
-                <Emphasis />
+                {/* <Emphasis /> */}
                 <MediaGallery />
                 <Outlet />
             </div>
